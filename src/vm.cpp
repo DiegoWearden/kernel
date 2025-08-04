f38f0f285105d298feed6efe81454cfb1ce5c690
DiegoWearden <diegowearden@utexas.edu>
2025-08-03 22:49:06 -0500

    simplified get_memory_attributes function

--- src/vm.cpp
+++ src/vm.cpp
@@ -77,15 +77,11 @@ static uint64_t* allocate_pmd_table() {
 
 // Helper function to determine memory attributes for a physical address        
 static inline uint64_t get_memory_attributes(uint64_t phys_addr) {
-    if (phys_addr >= 0x80000 && phys_addr < 0x40000000) {
+    if (phys_addr < 0x3F000000) {
         return PTE_SHARED | PTE_AP_RW | PTE_ATTRINDX_NORMAL;
     }
     
     if (phys_addr >= 0x40000000 && phys_addr < 0x40001000) {
-        return PTE_AP_RW | PTE_UXN | PTE_PXN | PTE_ATTRINDX_DEVICE;
-    }
-    
-    if (phys_addr >= 0x3C000000 && phys_addr < 0x3F000000) {
         return PTE_AP_RW | PTE_UXN | PTE_PXN | PTE_ATTRINDX_DEVICE;
     }
     
