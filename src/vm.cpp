4242181018eb5530a9dd25a1ce99fe99e76f10a4
DiegoWearden <diegowearden@utexas.edu>
2025-08-09 22:32:39 -0500

    typo

--- src/vm.cpp
+++ src/vm.cpp
@@ -85,11 +85,6 @@ static inline uint64_t get_memory_attributes(uint64_t phys_addr) {
     return PTE_SHARED | PTE_AP_RW | PTE_ATTRINDX_NORMAL;
 }
 
-// Export a helper for normal cached page attributes for heap expansion
-uint64_t vm_get_normal_page_attrs() {
-    return PTE_SHARED | PTE_AP_RW | PTE_ATTRINDX_NORMAL;
-}
-
 // Map a 2MB block
 bool map_address_2mb(uint64_t virt_addr, uint64_t phys_addr, uint64_t attrs) {
     const uint64_t BLOCK_SIZE = 2 * 1024 * 1024;
@@ -279,7 +274,7 @@ void create_page_tables() {
         map_address_4kb(virt_addr, virt_addr, get_memory_attributes(virt_addr));
     }
     
-    // Map low address space (0x0 - 0x40000000) - identity mapping
+    // Map low address space (0x0 + PAGE_SIZE_2MB - 0x40000000) - identity mapping
     for (uint64_t virt_addr = 0x0 + PAGE_SIZE_2MB; virt_addr < 0x40000000; virt_addr += PAGE_SIZE_2MB) {
         map_address_2mb(virt_addr, virt_addr, get_memory_attributes(virt_addr));
     }
